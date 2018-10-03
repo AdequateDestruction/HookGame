a7142ff61e7bb4940b3dc2d8b5bb9d57aedb9ca9
MarkoKii <markokiiskinen@kamk.fi>
2018-10-03 14:50:13 +0300

    Leaderboard UI and overall functionality, names, score etc display

--- Assets/Scripts/PlayFab/PlayFabSM.cs
+++ Assets/Scripts/PlayFab/PlayFabSM.cs
@@ -3,27 +3,45 @@ using System.Collections.Generic;
 using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class PlayFabSM : MonoBehaviour
 {
+    List<string> scoreList;
+    string userGivenName = "Pertti Jorma"; //name must be between 3-15 characters
+    int playerScoreInt = 7;
+    public double time= 00.2045;
+
+    GameObject leaderboardScoresObj;
+    GameObject leaderboardTitleObj;
+    GameObject leaderboardNamesObj;
+    public List<Text> texts = new List<Text>();
+    public List<PlayerLeaderboardEntry> playerScores = new List<PlayerLeaderboardEntry>();
+
+
     public void Start()
     {
-        //Note: Setting title Id here can be skipped if you have set the value in Editor Extensions already.
-        /*if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
-        {
-            PlayFabSettings.TitleId = "97B5"; // Please change this value to your own titleId from PlayFab Game Manager
-        }*/
-        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        leaderboardScoresObj = GameObject.FindGameObjectWithTag("LeaderboardScores");
+        leaderboardTitleObj = GameObject.FindGameObjectWithTag("LeaderboardTitle");
+        leaderboardNamesObj = GameObject.FindGameObjectWithTag("LeaderboardNames");
+        
+        var request = new LoginWithCustomIDRequest { CustomId = "AdequateDestruction", CreateAccount = true };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
     }
 
     private void OnLoginSuccess(LoginResult result)
     {
         Debug.Log("Congratulations, you made your first successful API call!");
-        UpdatePlayerStatistics();
-        GetPlayerStatistics();
+        //UpdatePlayerStatistics();
+        //GetPlayerStatistics();
+        //UpdateDisplayName();
+        GetLeaderboardScores();
     }
 
+
+    //Debug for if the login fails
     private void OnLoginFailure(PlayFabError error)
     {
         Debug.LogWarning("Something went wrong with your first API call.  :(");
@@ -31,18 +49,67 @@ public class PlayFabSM : MonoBehaviour
         Debug.LogError(error.GenerateErrorReport());
     }
 
+    //fetches leaderboard information for the specified board and version.
+    public void GetLeaderboardScores()
+    {
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest()
+        {
+            ProfileConstraints = null,
+            Version = 1,
+            StatisticName = "Score",
+            StartPosition = 0,
+            MaxResultsCount = 10,
+
+        },
+            result => setplayerScores(result.Leaderboard),
+            error => Debug.Log(error.GenerateErrorReport())            
+        );
+
+        
+    }
+    //sets the 'results' leaderboard info to a non-local variable so that it can be used elsewhere
+    public void setplayerScores(List<PlayerLeaderboardEntry> playerLeaderboardEntries)
+    {
+        print("playerleaderboardsentires count " + playerLeaderboardEntries.Count);
+        playerScores = playerLeaderboardEntries;
+        print("Playerscores count " + playerScores.Count);
+
+        showScores();
+    }
+    //shows the scores, altering the MaxResultCount will determine how many highscores are shown.
+    public void showScores()
+    {
+        print("playerScores count " + playerScores.Count);
+
+        if (playerScores != null)
+        {
+            leaderboardTitleObj.GetComponent<Text>().text = "halp halp"; //make this somehow search the name automatically
+
+            for (int i = 0; i < playerScores.Count; i++)
+            {
+                print("Leaderboard UI score change " + playerScores[0].StatValue);
+                leaderboardScoresObj.GetComponentsInChildren<Text>()[i].text = playerScores[i].StatValue.ToString();
+                leaderboardNamesObj.GetComponentsInChildren<Text>()[i].text = playerScores[i].DisplayName;
+
+            }
+        }
+    }
+
+
+    //getting player specific statistic/score
     public void GetPlayerStatistics()
     {
         PlayFabClientAPI.GetPlayerStatistics(
             new GetPlayerStatisticsRequest()
             {
-                StatisticNames = new List<string>() { "Score" }
+                StatisticNames = new List<string> { "PlayerName" },
             },
-            result => Debug.Log("Complete"),
+            result =>Debug.Log("Complete " + result.Statistics[0].Value),
             error => Debug.Log(error.GenerateErrorReport())
         );
     }
 
+    //for saving a new score, manual check needed so only the highest score is submitted to playfab
     public void UpdatePlayerStatistics()
     {
         PlayFabClientAPI.UpdatePlayerStatistics(
@@ -50,13 +117,26 @@ public class PlayFabSM : MonoBehaviour
             {
                 Statistics = new List<StatisticUpdate>() {
                 new StatisticUpdate() {
-                    StatisticName = "Score",
+                    StatisticName = "PlayerName",
                     Version = null,
-                    Value = 20
+                    Value = playerScoreInt
                 }
                 }
             },
             result => Debug.Log("Complete"),
+            error => Debug.Log(error.GenerateErrorReport())
+        );   
+    }
+
+    //sets the playername for the user so that it can be shown in the leaderboard, name must be between 3 -25 characters
+    public void UpdateDisplayName()
+    {
+        PlayFabClientAPI.UpdateUserTitleDisplayName(
+            new UpdateUserTitleDisplayNameRequest()
+            {
+                DisplayName = userGivenName
+            },
+            result => Debug.Log("Displayname changed"),
             error => Debug.Log(error.GenerateErrorReport())
         );
     }
