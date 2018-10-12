ce5eea0855d03029a0fff4b547a82e87b912570d
MarkoKii <markokiiskinen@kamk.fi>
2018-10-12 12:02:20 +0300

    Boss3 Stage Machine started 1st and 2nd phase kinda done, made more shadow prefabs for different projectiles and gave the minivolcanoes bools to stop shooting and to mark an enemy as a boss (so it can't be one shot with the hook)

--- Assets/Scripts/3rdBossExclusives/MiniVolcanoes.cs
+++ Assets/Scripts/3rdBossExclusives/MiniVolcanoes.cs
@@ -11,19 +11,21 @@ public class MiniVolcanoes : MonoBehaviour {
     public Animator tentacleAnimator;
     public Collider2D damageCollider;
     public Collider2D blockCollider;
-    public GameObject volcanoProjectile;
+    public GameObject projectileShadow;
 
     //public variables for balancing
 
 
 
     public float maxRange = 3, minRange = -3, randomInvokeMin = 0.05f, randomInvokeMax = 0.9f, invokeRepeat = 1;
-
+    public bool stopShooting, boss = false;
 
 
 
     AudioSource despawnSource;
     bool despawning;
+    
+
 
     PlayerMovement playerMovement;
 
@@ -45,7 +47,7 @@ public class MiniVolcanoes : MonoBehaviour {
 
     public void hookHit()
     {
-        if (!despawning)
+        if (!despawning && !boss)
         {
             despawnSource.Play();
             StartCoroutine(TentacleDeSpawn());
@@ -75,12 +77,16 @@ public class MiniVolcanoes : MonoBehaviour {
     {
         print("TacticalArtillery");
 
-        float xRandom = Random.Range(minRange, maxRange);
-        float yRandom = Random.Range(minRange, maxRange);
-        Vector2 spawnPos = new Vector2(playerMovement.transform.position.x + xRandom, playerMovement.transform.position.y + yRandom);
-        newProjectile = Instantiate(volcanoProjectile);
-        newProjectile.GetComponent<SpriteRenderer>().color = new Color(newProjectile.GetComponent<SpriteRenderer>().color.r, newProjectile.GetComponent<SpriteRenderer>().color.g, newProjectile.GetComponent<SpriteRenderer>().color.b, 0); //this line is horrible
-        newProjectile.transform.position = spawnPos;
+        if(stopShooting == false)
+        {
+            float xRandom = Random.Range(minRange, maxRange);
+            float yRandom = Random.Range(minRange, maxRange);
+            Vector2 spawnPos = new Vector2(playerMovement.transform.position.x + xRandom, playerMovement.transform.position.y + yRandom);
+            newProjectile = Instantiate(projectileShadow);
+            newProjectile.GetComponent<SpriteRenderer>().color = new Color(newProjectile.GetComponent<SpriteRenderer>().color.r, newProjectile.GetComponent<SpriteRenderer>().color.g, newProjectile.GetComponent<SpriteRenderer>().color.b, 0); //this line is horrible
+            newProjectile.transform.position = spawnPos;
+        }
+
 
     }
 
--- Assets/Scripts/3rdBossExclusives/ThirdBossSM.cs
+++ Assets/Scripts/3rdBossExclusives/ThirdBossSM.cs
@@ -4,13 +4,71 @@ using UnityEngine;
 
 public class ThirdBossSM : MonoBehaviour {
 
+    string currentState= "1stState";
+    MiniVolcanoes thisVolcano;
+    bool coroutineInProgress = false;
+
+
+    public GameObject MiniVolcParent;
+    public GameObject secondPhaseAmmunition;
+
 	// Use this for initialization
 	void Start () {
-		
+        thisVolcano = gameObject.GetComponent<MiniVolcanoes>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        switch (currentState)
+        {
+            case "1stState":
+                CheckVolcanoes();
+                break;
+            case "BustinOut":
+                if(coroutineInProgress == false)
+                {
+                    print("Bustin out coroutine started");
+                    StartCoroutine("BustinOut");
+                }
+                break;
+            case "2ndState":
+                SecondState();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void CheckVolcanoes()
+    {
+        if(MiniVolcParent.GetComponentInChildren<MiniVolcanoes>() == null)//Kaikki mini volcanot on tuhottu
+        {
+            print("all mini volcanoes dead");
+            //voi tuhota sen isomman volcanon
+            thisVolcano.stopShooting = true;
+            currentState = "BustinOut";
+        }
+    }
+
+    IEnumerator BustinOut() //Boss busts out of the volcano
+    {
+        coroutineInProgress = true;
+        print("BustinOut");
+
+        new WaitForSeconds(4f);
+        currentState = "2ndState";
+        StopCoroutine("BustinOut");
+        yield return null;
+    }
+
+    void SecondState()
+    {
+        if(thisVolcano.projectileShadow != secondPhaseAmmunition)
+        {
+            thisVolcano.projectileShadow = secondPhaseAmmunition;
+        }
+
+        thisVolcano.stopShooting = false;
+    }
 }
