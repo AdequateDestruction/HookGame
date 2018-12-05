ac1fee1e0f4474ba9f85b02ff22aa82aaba779a9
MarkoKii <markokiiskinen@kamk.fi>
2018-12-05 12:56:14 +0200

    New Animations, new volcano sprites, fire platform added, final stage art added and fixes and stuff

---
+++ Assets/Scripts/3rdBossExclusives/LavaPoolTriggerDMG.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaPoolTriggerDMG : MonoBehaviour {
+
+    PlayerMovement player;
+    bool touchingBoss = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (touchingBoss)
+            player.TakeDamage();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //Boss deals damage to player on contact
+    {
+        if (collision.transform.tag == "Player")
+        {
+            touchingBoss = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            
+        if (collision.transform.tag == "Player")
+        {
+            touchingBoss = false;
+        }
+    
+    }  //Boss deals damage to player on contact
+
+}
